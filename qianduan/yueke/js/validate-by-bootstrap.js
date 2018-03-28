2fd960cd03dfb09e320a874b49b2bc11aae2f35f
wanghui0713 <1010533151@qq.com>
2018-03-28 10:08:26 +0800

    前端

---
+++ qianduan/yueke/js/validate-by-bootstrap.js
@@ -0,0 +1,19 @@
+$.validator.setDefaults({
+    highlight: function (element) {
+        $(element).closest('.form-group').removeClass('has-success').addClass('has-error');
+    },
+    success: function (element) {
+        // element.closest('.form-group').removeClass('has-error').addClass('has-success');
+        element.closest('.form-group').removeClass('has-error')
+    },
+    errorElement: "span",
+    errorPlacement: function (error, element) {
+        if (element.is(":radio") || element.is(":checkbox")) {
+            error.appendTo(element.parent().parent().parent());
+        } else {
+            error.appendTo(element.parent());
+        }
+    },
+    errorClass: "help-block m-b-none",
+    validClass: "help-block m-b-none"
+});
